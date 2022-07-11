b77543fb65cd9eee3c757f9e346e53ef00b65ffa
mkellner43 <matthew.kellner@outlook.com>
2022-07-11 17:02:47 -0500

    fixed database bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article = Article.find(params[:id])
-    @article.destory
+    @article.destroy
 
     redirect_to root_path, status: :see_other
   end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
   include Visible
   has_many :comments, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, presence: true 
   validates :body, presence: true, length: { minimum: 10 }
 end
